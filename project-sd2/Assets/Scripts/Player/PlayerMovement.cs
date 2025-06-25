fa8eb2d56c6ec07a4b24c6f24bae786830242435
DusaPraharsh <dusapraharsh123@gmail.com>
2025-06-26 01:41:02 +0530

    cinemachine and anim init

--- project-sd2/Assets/Scripts/Player/PlayerMovement.cs
+++ project-sd2/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,45 @@
+using Unity.Mathematics.Geometry;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [Header("References")]
+    public CharacterController controller;
+    public Transform cam;
+    Animator anim;
+
+    [Header("Movement")]
+    public float walkSpeed;
+    public float sprintSpeed;
+
+    public float turnSmoothTime;
+    float turnSmoothVelocity;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * walkSpeed * Time.deltaTime);
+            anim.SetBool("walk", true);
+            if (direction.magnitude >= 0.1f && Input.GetKey(KeyCode.LeftShift))
+            {
+                controller.Move(moveDirection.normalized * sprintSpeed * Time.deltaTime);
+                anim.SetBool("sprint", true);
+            }
+        }
+    }
 }
